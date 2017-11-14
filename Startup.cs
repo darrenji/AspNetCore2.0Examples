dcdc60c19a4767090de0fa44824b06ba978d7904
darren <764190362@qq.com>
2017-11-14 22:10:38 +0800

    在Startup中的Configure方法中，使用IApplicationBuilder的Run方法直接运行中间件

--- Startup.cs
+++ Startup.cs
@@ -28,15 +28,11 @@ namespace Examples
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
+            app.Run(async (context) => {
                 await context.Response.WriteAsync("Hello World!");
             });
         }
+
+       
     }
 }
