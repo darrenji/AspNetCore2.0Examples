c2419728f97ed91df1ab309b4d8ac66361b00988
darren <764190362@qq.com>
2017-11-24 13:10:41 +0800

    Tag Helper, about cache, 未运行成功

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Examples.Models;
 using Newtonsoft.Json;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -17,58 +16,5 @@ namespace Examples.Controllers
             return View();
         }
 
-        public IActionResult Employees()
-        {
-            var model = new EmployeesViewModel {
-                Employees = new List<Employee> {
-                    new Employee
-                    {
-                        Name="darren",
-                        JobTitle="ceo",
-                        Profile="excellent",
-                        Friends=new List<Friend>
-                        {
-                            new Friend{Name="tom"},
-                            new Friend{ Name="dick"},
-                            new Friend{ Name="harry"}
-                        }
-                    },
-                    new Employee{
-                        Name="james",
-                        JobTitle="ceo",
-                        Profile="good",
-                        Friends=new List<Friend>
-                        {
-                            new Friend{Name="james gordon"},
-                            new Friend{Name="robin hood"}
-                        }
-                    }
-                }
-            };
-            return View(model);
-        }
-
-        public IActionResult Movie()
-        {
-            var model = new MovieViewModel {
-                Title ="diamonds",
-                ReleaseYear="1971",
-                Director="guy",
-                Summary="pretty good",
-                Starts = new List<string> { "sean", "jill st"}
-            };
-            return View(model);
-        }
-
-        public IActionResult Context()
-        {
-            ViewBag.Greeting = "hello from view bag";
-            return View();
-        }
-
-        public IActionResult Greet()
-        {
-            return View();
-        }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -8,14 +8,13 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Configuration;
-using Examples.Models;
 using Microsoft.Extensions.FileProviders;
 using System.IO;
 using Microsoft.AspNetCore.Rewrite;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Routing;
-using Examples.Services;
+
 
 namespace Examples
 {
@@ -33,7 +32,9 @@ namespace Examples
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddScoped<IGreetingService, GreetingService>();
+            services.AddDistributedRedisCache(options => {
+                options.Configuration = "...";
+            });
             services.AddMvc();
            
         }
